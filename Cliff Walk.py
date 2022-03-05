93628f66033544f8211320cf4e104e1c95713f8d
Srirag Jayakumar <sriragjayakumar@gmail.com>
2022-03-05 15:14:14 +0000

    Add files via upload

---
+++ Cliff Walk.py
@@ -0,0 +1,256 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sat Mar  5 14:59:17 2022
+
+@author: srira
+"""
+
+
+import numpy as np
+import pandas as pd
+import matplotlib.pyplot as plt
+
+
+    
+
+def epsilon_greedy_policy(state, q_table, epsilon = 0.1):
+
+    decide_explore_exploit  = np.random.random()
+    
+    if(decide_explore_exploit < epsilon):
+        action = np.random.choice(4) # UP = 0, LEFT = 1, RIGHT = 2, DOWN = 3
+    else:
+        action = np.argmax(q_table[:, state]) # Choose the action with largest Q-value (state value)
+        
+    return action
+    
+
+def move_agent(agent, action):
+
+    # get position of the agent
+    (posX , posY) = agent
+    # UP 
+    if ((action == 0) and posX > 0):
+        posX = posX - 1
+    # LEFT
+    if((action == 1) and (posY > 0)):
+        posY = posY - 1
+    # RIGHT
+    if((action == 2) and (posY < 11)):
+        posY = posY + 1
+    # DOWN
+    if((action) == 3 and (posX < 3)):
+        posX = posX + 1
+    agent = (posX, posY)
+    
+    return agent
+
+
+def get_reward(state):
+
+    # game continues
+    game_end = False
+    # all states except cliff have -1 value
+    reward = -1
+    # goal state
+    if(state == 47):
+        game_end = True
+        reward = 10
+    # cliff
+    if(state >= 37 and state <= 46):
+        game_end = True
+        # Penalize the agent if agent encounters a cliff
+        reward = -100
+
+    return reward, game_end
+
+def update_qTable(q_table, state, action, reward, next_state_value, gamma_discount = 1, alpha = 0.1):
+    """   
+    Q(S, A) <- Q(S, A) + [alpha * (reward + (gamma * maxValue(Q(S', A'))) -  Q(S, A) ]
+    """
+    update_q_value = q_table[action, state] + alpha * (reward + (gamma_discount * next_state_value) - q_table[action, state])
+    q_table[action, state] = update_q_value
+
+    return q_table    
+
+
+
+def sarsa(num_episodes = 500, gamma_discount = 1, alpha = 0.1, epsilon = 0.1):
+
+    q_table = np.zeros((4, 48))
+    step_cache, reward_cache = [],[]
+
+    for episode in range(0, num_episodes):
+        agent = (3, 0) # starting from left down corner
+        game_end = False
+        reward_cum = 0 # cumulative reward of the episode
+        step_cum = 0 # keeps number of iterations untill the end of the game
+        # choose action using policy
+        state =  12*agent[0] + agent[1]
+        action = epsilon_greedy_policy(state, q_table)
+        while(game_end == False):
+            # move agent to the next state
+            agent = move_agent(agent, action)
+            step_cum += 1
+            # observe next state value
+            next_state = 12*agent[0] + agent[1]
+            # observe reward and determine whether game ends
+            reward, game_end = get_reward(next_state)
+            reward_cum += reward 
+            # choose next_action using policy and next state
+            next_action = epsilon_greedy_policy(next_state, q_table)
+            # update q_table
+            next_state_value = q_table[next_action][next_state] # differs from q-learning uses the next action determined by policy
+            q_table = update_qTable(q_table, state, action, reward, next_state_value, gamma_discount, alpha)
+            # update the state and action
+            state = next_state
+            action = next_action # differs q_learning both state and action must updated
+        reward_cache.append(reward_cum)
+        step_cache.append(step_cum)
+        if(episode > 498):
+            print("Agent trained with SARSA after 500 iterations")
+
+    return q_table, reward_cache, step_cache
+
+def qlearning(num_episodes = 500, gamma_discount = 1, alpha = 0.1, epsilon = 0.1):
+
+    # initialize all states to 0
+    # Terminal state cliff_walking ends
+    reward_cache = list()
+    step_cache = list()
+    q_table = np.zeros((4, 48))
+    agent = (3, 0) # starting from left down corner
+    # start iterating through the episodes
+    for episode in range(0, num_episodes):
+        env = np.zeros((4, 12))
+
+        agent = (3, 0) # starting from left down corner
+        game_end = False
+        reward_cum = 0 # cumulative reward of the episode
+        step_cum = 0 # keeps number of iterations untill the end of the game
+        while(game_end == False):
+            # get the state from agent's position
+            state = 12*agent[0] + agent[1]
+            # choose action using epsilon-greedy policy
+            action = epsilon_greedy_policy(state, q_table)
+            # move agent to the next state
+            agent = move_agent(agent, action)
+            step_cum += 1
+
+            # observe next state value
+            next_state = 12*agent[0] + agent[1]
+            max_next_state_value = np.amax(q_table[:, int(state)])
+
+            # observe reward and determine whether game ends
+            reward, game_end = get_reward(next_state)
+            reward_cum += reward 
+            # update q_table
+            q_table = update_qTable(q_table, state, action, reward, max_next_state_value, gamma_discount, alpha)
+            # update the state
+            state = next_state
+        reward_cache.append(reward_cum)
+        if(episode > 498):
+            print("Agent trained with Q-learning after 500 iterations")
+            print(env) # display the last 2 path agent takes 
+        step_cache.append(step_cum)
+    return q_table, reward_cache, step_cache
+    
+def plot_cumreward_normalized(reward_cache_qlearning, reward_cache_SARSA):
+    """
+    Visualizes the reward convergence
+    
+    Args:
+        reward_cache -- type(list) contains cumulative_reward
+    """
+    cum_rewards_q = []
+    rewards_mean = np.array(reward_cache_qlearning).mean()
+    rewards_std = np.array(reward_cache_qlearning).std()
+    count = 0 # used to determine the batches
+    cur_reward = 0 # accumulate reward for the batch
+    for cache in reward_cache_qlearning:
+        count = count + 1
+        cur_reward += cache
+        if(count == 10):
+            # normalize the sample
+            normalized_reward = (cur_reward - rewards_mean)/rewards_std
+            cum_rewards_q.append(normalized_reward)
+            cur_reward = 0
+            count = 0
+            
+    cum_rewards_SARSA = []
+    rewards_mean = np.array(reward_cache_SARSA).mean()
+    rewards_std = np.array(reward_cache_SARSA).std()
+    count = 0 # used to determine the batches
+    cur_reward = 0 # accumulate reward for the batch
+    for cache in reward_cache_SARSA:
+        count = count + 1
+        cur_reward += cache
+        if(count == 10):
+            # normalize the sample
+            normalized_reward = (cur_reward - rewards_mean)/rewards_std
+            cum_rewards_SARSA.append(normalized_reward)
+            cur_reward = 0
+            count = 0      
+    # prepare the graph    
+    plt.plot(cum_rewards_q, label = "q_learning")
+    plt.plot(cum_rewards_SARSA, label = "SARSA")
+    plt.ylabel('Cumulative Rewards')
+    plt.xlabel('Batches of Episodes (sample size 10) ')
+    plt.title("Q-Learning/SARSA Convergence of Cumulative Reward")
+    plt.legend(loc='lower right', ncol=2, mode="expand", borderaxespad=0.)
+    plt.show()
+    
+def plot_number_steps(step_cache_qlearning, step_cache_SARSA):
+    """
+        Visualize number of steps taken
+    """    
+    cum_step_q = []
+    steps_mean = np.array(step_cache_qlearning).mean()
+    steps_std = np.array(step_cache_qlearning).std()
+    count = 0 # used to determine the batches
+    cur_step = 0 # accumulate reward for the batch
+    for cache in step_cache_qlearning:
+        count = count + 1
+        cur_step += cache
+        if(count == 10):
+            # normalize the sample
+            normalized_step = (cur_step - steps_mean)/steps_std
+            cum_step_q.append(normalized_step)
+            cur_step = 0
+            count = 0
+            
+    cum_step_SARSA = []
+    steps_mean = np.array(step_cache_SARSA).mean()
+    steps_std = np.array(step_cache_SARSA).std()
+    count = 0 # used to determine the batches
+    cur_step = 0 # accumulate reward for the batch
+    for cache in step_cache_SARSA:
+        count = count + 1
+        cur_step += cache
+        if(count == 10):
+            # normalize the sample
+            normalized_step = (cur_step - steps_mean)/steps_std
+            cum_step_SARSA.append(normalized_step)
+            cur_step = 0
+            count = 0      
+    # prepare the graph    
+    plt.plot(cum_step_q, label = "q_learning")
+    plt.plot(cum_step_SARSA, label = "SARSA")
+    plt.ylabel('Number of iterations')
+    plt.xlabel('Batches of Episodes (sample size 10) ')
+    plt.title("Q-Learning/SARSA Iteration number untill game ends")
+    plt.legend(loc='lower right', ncol=2, mode="expand", borderaxespad=0.)
+    plt.show()
+    
+
+def main():
+
+    q_table_SARSA, reward_cache_SARSA, step_cache_SARSA = sarsa()
+    q_table_qlearning, reward_cache_qlearning, step_cache_qlearning = qlearning()
+    plot_cumreward_normalized(reward_cache_qlearning,reward_cache_SARSA)
+    
+
+    
+if __name__ == "__main__":
+    # call main function to execute grid world
+    main()
